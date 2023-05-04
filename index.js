2ab92ab05bd705dac74b4ef8a33e9d19a158157a
NicTron <nicolasltroncoso@gmail.com>
2023-05-03 21:42:30 -0400

    First Real Question Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
